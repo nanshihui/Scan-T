25269d6fc1343153798619fbb35e1ae079f1c932
nanshihui@qq.com <nanshihui@qq.com>
2015-10-14 19:41:18 +0800

    add new files

--- SQLtool.py
+++ SQLtool.py
@@ -5,26 +5,29 @@ import MySQLdb
 class DBmanager:
         __cur=''
         __conn=''
-	def connectdb():
+        @classmethod
+	def connectdb(cls):
  		try:
-   			__conn=MySQLdb.connect(host='localhost',user='root',passwd='123456',db='datap',port=3306)
-    			__cur=__conn.cursor()
+   			cls.__conn=MySQLdb.connect(host='localhost',user='root',passwd='123456',db='datap',port=3306)
+    			cls.__cur=cls.__conn.cursor()
 
     		
     			print "success connet "
 		except MySQLdb.Error,e:
      			print "Mysql Error %d: %s" % (e.args[0], e.args[1])
-	def inserdata():
+        @classmethod
+	def inserdata(cls):
 		count=__cur.execute('select * from webdata')
 		print 'there has %s rows record' % count
       
-   		__cur.execute('insert into webdata(address,content,meettime) values(%s,%s,%s)',['www','123123','1992-12-12 12:12:12'])
+   		cls.__cur.execute('insert into webdata(address,content,meettime) values(%s,%s,%s)',['www','123123','1992-12-12 12:12:12'])
    		
-		result=__cur.fetchall()
+		result=cls.__cur.fetchall()
 		for temp in result:
     			print temp[0]
     		
-    		__conn.commit()
-	def closedb():
-    		__cur.close()
-    		__conn.close()
+    		cls.__conn.commit()
+        @classmethod
+	def closedb(cls):
+    		cls.__cur.close()
+    		cls.__conn.close()
