8263364eb49e8a1a8fcbf1e682a6364432b350ad
nanshihui@qq.com <nanshihui@qq.com>
2015-10-16 14:06:25 +0800

    add file

--- main.py
+++ main.py
@@ -18,17 +18,28 @@ def getImg(html):
 #html = getHtml("http://www.baidu.com")
 
 #print getImg(html)
+#while true:
+#	interpert()
+#	print 'please input the item you want: ',
+
+def interpert():
+	print''' welcome to use '''
+	print''' 1.search book by id  '''
+	print''' 2.search book by name  '''
+	print''' 3.search book by   '''
+	print''' '''
+interpert()
 people=SQLtool.DBmanager()
 people.connectdb()
 #people.showdata()
-#people.showtableinfo('book',['bid','title','author','press','price'])
+people.showtableinfo('book',['bid','title','author','press','price'])
 #people.searchtableinfo('book','bid','1001')
 #people.searchtableinfo_byitem(['users','orders'],['oid','user','name','ordertime','orders.state','payment'],['uid','user'],'102')
 #people.searchtableinfo_byitemmore(['users','book','orders','orderbook'],['orderid','bookid','title','author','press','quantity','user','name','ordertime'],['','bookid'],'1001')
 #people.searchtableinfo_byitemmore(['users','book','orders','orderbook'],['orderid','bookid','title','author','press','quantity','user','name','ordertime'],['','title'],'经济学原理')
 
 #people.searchtableinfo_bystate(['users','book','orders','orderbook'],['orderid','bookid','title','author','press','quantity','user','name','ordertime','orders.state'],['','bookid'],('1001',4))
-people.searchtableinfo_bystate(['users','book','orders','orderbook'],['orderid','bookid','title','author','press','quantity','user','name','ordertime','orders.state'],['',],(4))
+#people.searchtableinfo_bystate(['users','book','orders','orderbook'],['orderid','bookid','title','author','press','quantity','user','name','ordertime','orders.state'],['',],(4))
 #people.inserdata()
 people.closedb()
 
