ab52d6368879ab060a40c08b81d31ed7ab7438f2
nanshihui@qq.com <nanshihui@qq.com>
2015-10-19 23:32:34 +0800

    修正制表符

--- src/gzipsupport.py
+++ src/gzipsupport.py
@@ -4,9 +4,9 @@ import urllib2
 from gzip import GzipFile
 from StringIO import StringIO
 class ContentEncodingProcessor(urllib2.BaseHandler):
-  	"""
-  		A handler to add gzip capabilities to urllib2 requests 
-  	"""
+	"""
+		A handler to add gzip capabilities to urllib2 requests 
+	"""
  
 	# add headers to requests
  	def http_request(self, req):
@@ -14,7 +14,7 @@ class ContentEncodingProcessor(urllib2.BaseHandler):
 		print '添加gzip,deflate支持'
 		return req
  
-  	# decode
+	# decode
 	def http_response(self, req, resp):
 		old_resp = resp
 	# gzip
@@ -23,18 +23,18 @@ class ContentEncodingProcessor(urllib2.BaseHandler):
 			gz = GzipFile(fileobj=StringIO(resp.read()),mode="r")
 			resp = urllib2.addinfourl(gz, old_resp.headers, old_resp.url, old_resp.code)
 			resp.msg = old_resp.msg
-  	 # deflate
+	 # deflate
 		if resp.headers.get("content-encoding") == "deflate":
 			print '解码deflate'
 			gz = StringIO( deflate(resp.read()) )
-			resp = urllib2.addinfourl(gz, old_resp.headers, old_resp.url, old_resp.code)  # 'class to add info() and
+			resp = urllib2.addinfourl(gz, old_resp.headers, old_resp.url, old_resp.code)# 'class to add info() and
 			resp.msg = old_resp.msg
 		return resp
  
 # deflate support
 import zlib
-def deflate(data):   # zlib only provides the zlib compress format, not the deflate format;
-	try:               # so on top of all there's this workaround:
+def deflate(data): # zlib only provides the zlib compress format, not the deflate format;
+	try: # so on top of all there's this workaround:
 		return zlib.decompress(data, -zlib.MAX_WBITS)
 	except zlib.error:
 		return zlib.decompress(data)
