ad39cc66e67b63d65492541316da230a6bd7225b
nanshihui@qq.com <nanshihui@qq.com>
2015-10-28 22:40:33 +0800

    添加网页分析模块

--- src/spidertool/dealTask.py
+++ src/spidertool/dealTask.py
@@ -6,18 +6,38 @@ import time
 import connectpool
 from TaskTool import TaskTool
 import SQLTool
-class dealTask(TaskTool):
+import HTMLParser
+import re
+
+	
+class dealTask(TaskTool,HTMLParser.HTMLParser):
 	def __init__(self):
 		TaskTool.__init__(self)
+		HTMLParser.HTMLParser.__init__(self)
 		self.connectpool=connectpool.ConnectPool()
+
 	def task(self,req,threadname):
 		print threadname+'执行任务中'+str(datetime.datetime.now())
-		ans = req[0];
+		ans = self.makesqlit(req[1])
+
 
 		print threadname+'任务结束'+str(datetime.datetime.now())
 		return ans
-
+	#该方法用来处理开始标签的，eg:<div id="main">  
+	def handle_starttag(self, tag, attrs):  
+		if tag == 'a':  #如果为<a>标签  
+	#name为标签的属性名，如href、name、id、onClick等等  
+			for name,value in attrs:      
+				if name == 'href': #这时选择href属性  
+					print "name_value: ",value  #href属性的值  
+					print "first tag:",self.get_starttag_text() #<a>标签的开始tag  
+					print "\n"
+	def makesqlit(self,content):
+		self.feed(content)
+		self.close()
+#		return content
 if __name__ == "__main__":
+	"""
 	DealSQL=SQLTool.DBmanager()
 	DealSQL.connectdb()
 	(result,title,count,col)=DealSQL.searchtableinfo_byparams(['webdata'],['address','content','meettime'])
@@ -38,6 +58,18 @@ if __name__ == "__main__":
 		print ans
 	while True:
 		pass
+
+	"""
+
+	try:
+		file_object = open('test.html')
+		content = file_object.read( )
+		file_object.close( )
+	except Exception,e:
+		print e
+	TOOL=dealTask()
+	TOOL.makesqlit(content)
+
 
 
 
