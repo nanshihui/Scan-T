e1b48188f308a48f13040533c9cccce672d19df9
nanshihui@qq.com <nanshihui@qq.com>
2015-11-01 19:07:38 +0800

    队列测试，主码未改

--- src/spidertool/test.py
+++ src/spidertool/test.py
@@ -1,11 +1,21 @@
 #!/usr/bin/python
+import Queue
 def aa():
 	print 'already in port'
 class bb:
 	def __init__(self):
+		self.yy=0
+		self.qq=Queue.Queue()
+		self.qq.put(self.aa)
+		self.yy=1
 		print 'alread user port'
+	def aa(self):
+		return self.yy
+	def cc(self):
+		u=self.qq.get()
+		print u()
 
 def asd():
 	return 1,2
-a=asd()
-print len(a)
+a=bb()
+a.cc()
