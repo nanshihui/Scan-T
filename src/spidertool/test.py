e985d7670e158042805e7aff12ee2a3e55d7dadd
nanshihui@qq.com <nanshihui@qq.com>
2015-11-02 19:15:31 +0800

    进程线程抽象类添加

--- src/spidertool/test.py
+++ src/spidertool/test.py
@@ -6,16 +6,16 @@ class bb:
 	def __init__(self):
 		self.yy=0
 		self.qq=Queue.Queue()
-		self.qq.put(self.aa)
+		self.qq.put((self.aa,1))
 		self.yy=1
 		print 'alread user port'
 	def aa(self):
 		return self.yy
 	def cc(self):
-		u=self.qq.get()
+		u,k=self.qq.get()
 		print u()
-
+		print k
 def asd():
 	return 1,2
-a=bb()
-a.cc()
+for l in range(0,6):
+	print l
