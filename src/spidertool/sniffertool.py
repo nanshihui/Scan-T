a3804e56723298f97fe0ce16afebdc1817c13212
nanshihui@qq.com <nanshihui@qq.com>
2015-11-03 15:08:03 +0800

    修改嗅探模块

--- src/spidertool/sniffertool.py
+++ src/spidertool/sniffertool.py
@@ -8,7 +8,9 @@ Created on 2015年10月29日
 '''
 
 import sys
-import nmap
+import nmap   
+reload(sys) # Python2.5 初始化后会删除 sys.setdefaultencoding 这个方法，我们需要重新载入   
+sys.setdefaultencoding('utf-8')   
 class SniffrtTool(object):
     '''
     classdocs
@@ -26,11 +28,12 @@ except nmap.PortScannerError:
 
 except:
     print('Unexpected error:', sys.exc_info()[0])
-
+"""
 nm.scan('127.0.0.1', '22-443')                                   # scan host 127.0.0.1, ports from 22 to 443
 nm.command_line()                                                   # get command line used for the scan : nmap -oX – -p 22-443 127.0.0.1
 nm.scaninfo()                                                               # get nmap scan informations {'tcp': {'services': ’22-443′, 'method': 'connect’}}
 nm.all_hosts()                                                               # get all hosts that were scanned
+"""
 """
 nm['127.0.0.1'].hostname()                                       # get hostname for host 127.0.0.1
 nm['127.0.0.1'].state()                                                 # get state of host 127.0.0.1 (up|down|unknown|skipped)
@@ -44,6 +47,7 @@ nm['127.0.0.1'].has_tcp(22)                                    # is there any in
 nm['127.0.0.1']['tcp'][22]                                         # get infos about port 22 in tcp on host 127.0.0.1
 nm['127.0.0.1'].tcp(22)                                            # get infos about port 22 in tcp on host 127.0.0.1
 nm['127.0.0.1']['tcp'][22]['state']                          # get state of port 22/tcp on host 127.0.0.1 (open
+"""
 """
 # a more usefull example :
 for host in nm.all_hosts():
@@ -64,12 +68,30 @@ hosts_list = [(x, nm[x]['status']['state']) for x in nm.all_hosts()]
 for host, status in hosts_list:
     print('{0}:{1}'.format(host, status))
 print '—————————————————-'
+"""
 # Asynchronous usage of PortScannerAsync
-nma = nmap.PortScannerAsync()
+
 def callback_result(host, scan_result):
     print '——————'
     print host, scan_result
-nma.scan(hosts='192.168.1.115/30', arguments='-sP', callback=callback_result)
+    tmp=scan_result
+    result=''
+    result = result + "ip地址:%s 主机名:[%s]  ......  %s\n" %(host,tmp['scan'][host]['hostname'],tmp['scan'][host]['status']['state'])
+    try:
+        ports = tmp['scan'][host]['tcp'].keys()
+        for port in ports:
+            info = ''
+            portinfo = "%s port : %s  state : %s  product : %s \n" %(info,port,tmp['scan'][host]['tcp'][port]['state'],   tmp['scan'][host]['tcp'][port]['product'])
+            result = result + portinfo
+    except IOError,e:
+        print e
+    print result
+def callback_resultl(host, scan_result):
+    print scan_result
+nma = nmap.PortScannerAsync()
+nma.scan(hosts='www.baidu.com', arguments='-v -A -sT ', callback=callback_resultl)
+
+
 while nma.still_scanning():
     print('Waiting …')
     nma.wait(2) 
