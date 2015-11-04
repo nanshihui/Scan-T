693a014af82043d74ab7615d0ecae411838fc2ed
nanshihui@qq.com <nanshihui@qq.com>
2015-11-04 22:31:46 +0800

    增加类

---
+++ src/spidertool/Job_item.py
@@ -0,0 +1,23 @@
+'''
+Created on 2015年11月4日
+
+@author: dell
+'''
+# !/usr/bin/env python
+# -*- coding:utf-8 -*-
+class Job_Item(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self,jobname='',jobaddress='',jobport='',priority=1,result=''):
+        '''
+        Constructor
+        '''
+        self.jobname=''
+        self.jobaddress=''
+        self.jobport=''
+        self.priority=1
+        self.result=''
+        
