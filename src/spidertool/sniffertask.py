e41ae4a4fba4d6a3936c8206c0dcf179f77c6268
nanshihui@qq.com <nanshihui@qq.com>
2015-11-07 13:37:04 +0800

    封装嗅探任务

--- src/spidertool/sniffertask.py
+++ src/spidertool/sniffertask.py
@@ -5,10 +5,11 @@ import datetime
 import time
 import connectpool
 from TaskTool import TaskTool
+from sniffertool import  SniffrtTool
 class snifferTask(TaskTool):
     def __init__(self,isThread=1):
         TaskTool.__init__(self,isThread)
-        self.connectpool=connectpool.ConnectPool()
+        self.sniffer=SniffrtTool()
     def task(self,req,threadname):
         print threadname+'执行任务中'+str(datetime.datetime.now())
         
@@ -17,7 +18,40 @@ class snifferTask(TaskTool):
         print threadname+'任务结束'+str(datetime.datetime.now())
         return ans
 
+def callback_resultl(host, scan_result):
+    print scan_result
+    print '——————'
+    tmp=scan_result
+    result=''
 
+    try:
+        result =  u"ip地址:%s 主机名:%s  ......  %s\n" %(host,tmp['scan'][host]['hostname'],tmp['scan'][host]['status']['state'])
+        result +=u"系统信息 ： %s %s %s   准确度:%s " % (str(tmp['scan'][host]['osclass']['vendor']),str(tmp['scan'][host]['osclass']['osfamily']),str(tmp['scan'][host]['osclass']['osgen']),str(tmp['scan'][host]['osclass']['accuracy']))
+        ports = tmp['scan'][host]['tcp'].keys()
+        for port in ports:
+
+            portinfo = " port : %s  name:%s  state : %s  product : %s version :%s  script:%s \n" %(port,tmp['scan'][host]['tcp'][port]['name'],tmp['scan'][host]['tcp'][port]['state'],   tmp['scan'][host]['tcp'][port]['product'],tmp['scan'][host]['tcp'][port]['version'],tmp['scan'][host]['tcp'][port]['script'])
+            result = result + portinfo
+    except Exception,e:
+        print e
+    except IOError,e:
+        print '错误IOError'+str(e)
+    except KeyError,e:
+        print '不存在该信息'+str(e)
+    finally:
+
+        try:
+            ports = tmp['scan'][host]['udp'].keys()
+            for port in ports:
+                portinfo = " port : %s  name:%s  state : %s  product : %s  version :%s  script:%s \n" %(port,tmp['scan'][host]['udp'][port]['name'],tmp['scan'][host]['udp'][port]['state'],   tmp['scan'][host]['udp'][port]['product'],tmp['scan'][host]['udp'][port]['version'],tmp['scan'][host]['udp'][port]['script'])
+                result = result + portinfo
+
+        except IOError,e:
+            print '错误IOError-udp'+str(e)
+        except KeyError,e:
+            print '不存在该信息-udp'+str(e)
+        finally:
+            print result
 
 
 
--- src/spidertool/sniffertool.py
+++ src/spidertool/sniffertool.py
@@ -10,6 +10,7 @@ Created on 2015年10月29日
 import sys
 import nmap   
 import os
+from numpy.numarray.numerictypes import IsType
 reload(sys) # Python2.5 初始化后会删除 sys.setdefaultencoding 这个方法，我们需要重新载入   
 class SniffrtTool(object):
     '''
@@ -24,51 +25,72 @@ class SniffrtTool(object):
         try:
             self.nm = nmap.PortScanner()                                     # instantiate nmap.PortScanner object
             self.nma = nmap.PortScannerAsync()
-            self.params='-A -P0   -Pn  -sC   '
+            self.params='-A -P0   -Pn  -sC  -R  '
 
         except nmap.PortScannerError:
             print('Nmap not found', sys.exc_info()[0])
 
         except:
             print('Unexpected error:', sys.exc_info()[0])
-    def scan(self,hosts='localhost', port='', callback=''):
+    def scan(self,hosts='localhost', port='', callback='',arguments=''):
         orders=''
         if port!='':
-            orders+=' -p '+port
+            orders+='   -p '+port
         if callback=='':
-            
-            self.nma.scan(hosts=hosts, arguments=self.params+orders, callback=self.callback_result)
+            print type(self.callback_result);
+            self.nma.scan(hosts=hosts, arguments=self.params+arguments+orders, callback=self.callback_result)
         else:
-            self.nma.scan(hosts=hosts, arguments=self.params+orders, callback=callback)   
+            self.nma.scan(hosts=hosts, arguments=self.params+arguments+orders, callback=callback)   
 
     def callback_result(self,host, scan_result):
+        print 'test'
         print scan_result
         
-    def scanaddress(self,hosts=[], ports=[], call_back=''):
+    def scanaddress(self,hosts=[], ports=[], call_back='',arguments=''):
         for i in range(len(hosts)):
             if len(ports)<=i:
-                self.scan(hosts=hosts[i], callback=call_back)
+                self.scan(hosts=hosts[i], callback=call_back,arguments=arguments)
             else:
                     
-                self.scan(hosts=hosts[i], port=ports[i], callback=call_back)
+                self.scan(hosts=hosts[i], port=ports[i], callback=call_back,arguments=arguments)
             
     def isrunning(self):
         return self.nma.still_scanning()
-def callback_result(host, scan_result):
+def callback_resultl(host, scan_result):
+    print scan_result
     print '——————'
-    print host, scan_result
     tmp=scan_result
     result=''
-    result = result + "ip地址:%s 主机名:[%s]  ......  %s\n" %(host,tmp['scan'][host]['hostname'],tmp['scan'][host]['status']['state'])
+
     try:
+        result =  u"ip地址:%s 主机名:%s  ......  %s\n" %(host,tmp['scan'][host]['hostname'],tmp['scan'][host]['status']['state'])
+        result +=u"系统信息 ： %s %s %s   准确度:%s " % (str(tmp['scan'][host]['osclass']['vendor']),str(tmp['scan'][host]['osclass']['osfamily']),str(tmp['scan'][host]['osclass']['osgen']),str(tmp['scan'][host]['osclass']['accuracy']))
         ports = tmp['scan'][host]['tcp'].keys()
         for port in ports:
-            info = ''
-            portinfo = "%s port : %s  state : %s  product : %s \n" %(info,port,tmp['scan'][host]['tcp'][port]['state'],   tmp['scan'][host]['tcp'][port]['product'])
+
+            portinfo = " port : %s  name:%s  state : %s  product : %s version :%s  script:%s \n" %(port,tmp['scan'][host]['tcp'][port]['name'],tmp['scan'][host]['tcp'][port]['state'],   tmp['scan'][host]['tcp'][port]['product'],tmp['scan'][host]['tcp'][port]['version'],tmp['scan'][host]['tcp'][port]['script'])
             result = result + portinfo
-    except IOError,e:
+    except Exception,e:
         print e
-    print result
+    except IOError,e:
+        print '错误IOError'+str(e)
+    except KeyError,e:
+        print '不存在该信息'+str(e)
+    finally:
+
+        try:
+            ports = tmp['scan'][host]['udp'].keys()
+            for port in ports:
+                portinfo = " port : %s  name:%s  state : %s  product : %s  version :%s  script:%s \n" %(port,tmp['scan'][host]['udp'][port]['name'],tmp['scan'][host]['udp'][port]['state'],   tmp['scan'][host]['udp'][port]['product'],tmp['scan'][host]['udp'][port]['version'],tmp['scan'][host]['udp'][port]['script'])
+                result = result + portinfo
+
+        except IOError,e:
+            print '错误IOError-udp'+str(e)
+        except KeyError,e:
+            print '不存在该信息-udp'+str(e)
+        finally:
+            print result
+    
 """
 def callback_resultl(host, scan_result):
     print scan_result
@@ -85,8 +107,8 @@ orderq='-A -P0   -Pn  -sC  -p '
 
 if __name__ == "__main__":   
     temp=SniffrtTool()
-    hosts=['127.0.0.1','www.baidu.com']
-    temp.scanaddress(hosts)
+    hosts=['www.bnuz.edu.cn']
+    temp.scanaddress(hosts,arguments='',call_back=callback_resultl)
     while temp.isrunning():
         temp.nma.wait(2)
 
