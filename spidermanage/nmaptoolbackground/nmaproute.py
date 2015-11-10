bc5f809de97a58ff22736762657ef3a195603e1a
nanshihui@qq.com <nanshihui@qq.com>
2015-11-10 16:14:59 +0800

    修正部分错误

--- spidermanage/nmaptoolbackground/nmaproute.py
+++ spidermanage/nmaptoolbackground/nmaproute.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import datetime
+from django.shortcuts import render_to_response
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world. You're at the nmaptool index.")
