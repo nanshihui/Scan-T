97fb608038e1a6ab1552959246d520f6d6f67132
nanshihui <nanshihui@qq.com>
2015-12-22 18:40:32 +0800

    添加数据库文件

---
+++ spidermanage/data/datap_snifferdata.sql
@@ -0,0 +1,53 @@
+CREATE DATABASE  IF NOT EXISTS `datap` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `datap`;
+-- MySQL dump 10.13  Distrib 5.5.46, for debian-linux-gnu (x86_64)
+--
+-- Host: 127.0.0.1    Database: datap
+-- ------------------------------------------------------
+-- Server version	5.5.46-0+deb8u1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `snifferdata`
+--
+
+DROP TABLE IF EXISTS `snifferdata`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `snifferdata` (
+  `ip` varchar(45) DEFAULT NULL,
+  `port` int(11) DEFAULT NULL,
+  `timesearch` varchar(45) DEFAULT NULL,
+  `state` varchar(45) DEFAULT NULL,
+  `name` varchar(45) DEFAULT NULL,
+  `product` varchar(45) DEFAULT NULL,
+  `version` varchar(45) DEFAULT NULL,
+  `script` text,
+  `detail` text,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `head` text,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `index2` (`ip`,`port`)
+) ENGINE=InnoDB AUTO_INCREMENT=2388 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-12-22 18:39:24
